3e7421b7ab6e992de047ad206b6b2ca3e0dc600a
Andrey Batulov <batulov.andrey.89@gmail.com>
2017-10-26 12:07:57 +0300

    added Retrofit, created ApiClient and Service for getting the data

--- app/src/main/java/com/github/batulovandrey/urbandictionarycom/MainActivity.java
+++ app/src/main/java/com/github/batulovandrey/urbandictionarycom/MainActivity.java
@@ -1,7 +1,18 @@
 package com.github.batulovandrey.urbandictionarycom;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.batulovandrey.urbandictionarycom.bean.BaseResponse;
+import com.github.batulovandrey.urbandictionarycom.bean.DefinitionResponse;
+import com.github.batulovandrey.urbandictionarycom.service.ApiClient;
+import com.github.batulovandrey.urbandictionarycom.service.UrbanDictionaryService;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * @author Andrey Batulov on 26/10/2017
@@ -13,5 +24,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        UrbanDictionaryService service = ApiClient.getRetrofit().create(UrbanDictionaryService.class);
+        Call<BaseResponse> call = service.getDefine("wat");
+        call.enqueue(new Callback<BaseResponse>() {
+            @Override
+            public void onResponse(Call<BaseResponse> call, Response<BaseResponse> response) {
+                if (response.body() != null) {
+                    List<DefinitionResponse> definitionResponses = response.body().getDefinitionResponses();
+                    for (DefinitionResponse definitionResponse : definitionResponses) {
+                        System.out.println(definitionResponse);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<BaseResponse> call, Throwable t) {
+                System.out.println(t.getMessage());
+            }
+        });
     }
 }
