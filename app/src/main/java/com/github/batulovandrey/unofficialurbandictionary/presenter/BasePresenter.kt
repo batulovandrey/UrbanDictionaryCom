e56208bc43037ce010e235bdf0e317477f717297
Andrey Batulov <batulov.andrey.89@gmail.com>
2018-07-26 15:42:32 +0300

    added MvpPresenter and MvpView as interfaces and BasePresenter class

---
+++ app/src/main/java/com/github/batulovandrey/unofficialurbandictionary/presenter/BasePresenter.kt
@@ -0,0 +1,33 @@
+package com.github.batulovandrey.unofficialurbandictionary.presenter
+
+import com.github.batulovandrey.unofficialurbandictionary.data.DataManager
+import io.reactivex.disposables.CompositeDisposable
+
+/**
+ * Base class that implements the Presenter interface and provides a base implementation for
+ * onAttach() and onDetach()
+ */
+open class BasePresenter<V : MvpView>(val dataManager: DataManager,
+                                 val compositeDisposable: CompositeDisposable) : MvpPresenter<V> {
+
+    var mvpView: V? = null
+        private set
+
+    override fun onAttach(mvpView: V) {
+        this.mvpView = mvpView
+    }
+
+    override fun onDetach() {
+        compositeDisposable.dispose()
+        mvpView = null
+    }
+
+    /**
+     * This method checks attached view.
+     * If it is attached, @return true or false if not
+     */
+    fun isViewAttached(): Boolean {
+        return mvpView != null
+    }
+
+}
---
+++ app/src/main/java/com/github/batulovandrey/unofficialurbandictionary/presenter/MvpView.kt
@@ -0,0 +1,12 @@
+package com.github.batulovandrey.unofficialurbandictionary.presenter
+
+interface MvpView {
+
+    fun showLoading()
+
+    fun hideLoading()
+
+    fun hideKeyboard()
+
+    fun isNetworkConnected(): Boolean
+}
