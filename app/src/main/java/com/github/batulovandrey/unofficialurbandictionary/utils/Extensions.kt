201c8228c8aa0374c56179a3c0eee3d977a2ad08
Andrey Batulov <batulov.andrey.89@gmail.com>
2018-08-02 17:39:09 +0300

    added the Extensions class to convert old objects to new ones

---
+++ app/src/main/java/com/github/batulovandrey/unofficialurbandictionary/utils/Extensions.kt
@@ -0,0 +1,60 @@
+package com.github.batulovandrey.unofficialurbandictionary.utils
+
+import com.github.batulovandrey.unofficialurbandictionary.bean.DefinitionResponse
+import com.github.batulovandrey.unofficialurbandictionary.bean.UserQuery
+import com.github.batulovandrey.unofficialurbandictionary.data.db.model.Definition
+import com.github.batulovandrey.unofficialurbandictionary.data.db.model.SavedUserQuery
+import io.realm.RealmResults
+
+fun List<DefinitionResponse>.convertToDefinitionList(): List<Definition> {
+
+    val result = ArrayList<Definition>()
+
+    for (item in this) {
+
+        val definition = Definition(null,
+                item.definition,
+                item.permalink,
+                item.thumbsUp,
+                item.thumbsDown,
+                item.author,
+                item.word,
+                item.example)
+
+        result.add(definition)
+
+    }
+
+    return result
+}
+
+fun RealmResults<DefinitionResponse>.convertToDefinitionList(): List<Definition> {
+    val result = ArrayList<Definition>()
+
+    for (item in this) {
+        val definition = Definition(null,
+                item.definition,
+                item.permalink,
+                item.thumbsUp,
+                item.thumbsDown,
+                item.author,
+                item.word,
+                item.example)
+
+        result.add(definition)
+    }
+
+    return result
+}
+
+fun RealmResults<UserQuery>.convertToUserQueriesList()
+        : List<SavedUserQuery> {
+    val result = ArrayList<SavedUserQuery>()
+
+    for (item in this) {
+        val userQuery = SavedUserQuery(null, item.query)
+        result.add(userQuery)
+    }
+
+    return result
+}
