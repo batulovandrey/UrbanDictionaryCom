6ba2eef9623f1ba134ed4c737fce2c30a5ad728d
Andrey Batulov <batulov.andrey.89@gmail.com>
2018-08-02 17:50:44 +0300

    fixed injections

--- app/src/main/java/com/github/batulovandrey/unofficialurbandictionary/dagger/NetModule.java
+++ app/src/main/java/com/github/batulovandrey/unofficialurbandictionary/dagger/NetModule.java
@@ -1,17 +1,24 @@
 package com.github.batulovandrey.unofficialurbandictionary.dagger;
 
 import android.app.Application;
+import android.content.Context;
 
+import com.github.batulovandrey.unofficialurbandictionary.UrbanDictionaryApp;
+import com.github.batulovandrey.unofficialurbandictionary.api.UrbanDictionaryService;
+import com.github.batulovandrey.unofficialurbandictionary.data.DataManager;
 import com.github.batulovandrey.unofficialurbandictionary.data.PopularWords;
 import com.github.batulovandrey.unofficialurbandictionary.realm.RealmManager;
-import com.github.batulovandrey.unofficialurbandictionary.api.UrbanDictionaryService;
+import com.github.batulovandrey.unofficialurbandictionary.ui.main.MainMvpPresenter;
+import com.github.batulovandrey.unofficialurbandictionary.ui.main.MainMvpView;
+import com.github.batulovandrey.unofficialurbandictionary.ui.main.MainPresenter;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import io.reactivex.disposables.CompositeDisposable;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -45,7 +52,7 @@ public class NetModule {
         return new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
     }
 
@@ -53,5 +60,29 @@ public class NetModule {
     @Singleton
     UrbanDictionaryService provideService(Retrofit retrofit) {
         return retrofit.create(UrbanDictionaryService.class);
+    }
+
+    @Provides
+    @Singleton
+    MainMvpPresenter<MainMvpView> provideMainPresenter(DataManager dataManager,
+                                                       CompositeDisposable disposable) {
+        return new MainPresenter<>(dataManager, disposable);
+    }
+
+    @Provides
+    CompositeDisposable provideDisposable() {
+        return new CompositeDisposable();
+    }
+
+    @Provides
+    @Singleton
+    DataManager provideDataManager(Application application) {
+        return ((UrbanDictionaryApp) application).getDataManager();
+    }
+
+
+    @Provides
+    Context provideContext(Application application) {
+        return application.getApplicationContext();
     }
 }
